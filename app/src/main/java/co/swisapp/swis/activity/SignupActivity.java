2d2dad82602da0ffa4e153eecffb42554a066107
abhinav-adtechs <abhinav.adtechs@gmail.com>
2015-12-29 16:25:50 +0530

    Several additions and refractoring Signup Page

--- app/src/main/java/co/swisapp/swis/activity/SignupActivity.java
+++ app/src/main/java/co/swisapp/swis/activity/SignupActivity.java
@@ -25,25 +25,20 @@ import co.swisapp.swis.R;
 import co.swisapp.swis.utility.Constants;
 
 public class SignupActivity extends AppCompatActivity implements View.OnClickListener{
-
-
     public String USERCHECK_URL ;
-
     private EditText etUserName;
     private EditText etEmail;
     private EditText etPassword;
 
     private Button bRegister;
+    boolean value ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        initialize();
-    }
-
-    private void initialize() {
+        /*Declaration of all the UI elements with onClickListener*/
         etUserName = (EditText) findViewById(R.id.username) ;
         etEmail = (EditText) findViewById(R.id.email_id) ;
         etPassword = (EditText) findViewById(R.id.password) ;
@@ -51,24 +46,73 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         bRegister = (Button) findViewById(R.id.register) ;
         bRegister.setOnClickListener(this);
 
+        //API call as soon as Username added or changed to check uniqueness
+
         etUserName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if(!hasFocus){
                     if(!usernameCheck()){
-                        //Update UI to modify username
-                        Toast.makeText(getApplicationContext(), R.string.username_taken, Toast.LENGTH_LONG).show();
+                           //Update UI to modify username
+                        etUserName.setBackgroundResource(R.drawable.signup_error_ui);
+                        Toast.makeText(getApplicationContext(), R.string.username_taken, Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        etUserName.setBackgroundResource(R.drawable.signup_ok_ui);
+
                     }
                 }
             }
         });
+        etEmail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    if(!getEmailfromUI().contains("@")){
+                        etEmail.setBackgroundResource(R.drawable.signup_error_ui);
+                        Toast.makeText(getApplicationContext(), R.string.invalid_Email, Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                        etEmail.setBackgroundResource(R.drawable.signup_ok_ui);
+
+                }
+            }
+        });
+        etPassword.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus){
+                    if (getPasswordfromUI().length() < 6) {
+                        Toast.makeText(getApplicationContext(), R.string.password_short, Toast.LENGTH_SHORT).show();
+                        etPassword.setBackgroundResource(R.drawable.signup_error_ui);
+                    }
+                    else if (getPasswordfromUI().contains(getUsernamefromUI())){
+                        Toast.makeText(getApplicationContext(), R.string.password_contains_username, Toast.LENGTH_SHORT).show();
+                        etPassword.setBackgroundResource(R.drawable.signup_error_ui);
+                    }
+                    else
+                        etPassword.setBackgroundResource(R.drawable.signup_ok_ui);
+                }
+            }
+        });
+
     }
 
-    /*Extraction of data as inserted by the user from UI */
-    final String username = etUserName.getText().toString().trim() ;
-    final String email = etEmail.getText().toString().trim() ;
-    final String password = etPassword.getText().toString().trim() ;
-
+    /*method of view.OnClickListener that is implemented*/
+    @Override
+    public void onClick(View v) {
+        if(v == bRegister) {
+            if(validityCheck()){
+                try {
+                    if(usernameCheck())
+                        registration();
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), R.string.error_common, Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
 
     /*Function to handle the entire registration*/
@@ -76,9 +120,9 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
 
         /*Setting parameters to POST call*/
         HashMap<String, String> params = new HashMap<>() ;
-        params.put(Constants.USERNAME, username) ;
-        params.put(Constants.EMAIL, email) ;
-        params.put(Constants.PASSWORD, password) ;
+        params.put(Constants.USERNAME, getUsernamefromUI()) ;
+        params.put(Constants.EMAIL, getEmailfromUI()) ;
+        params.put(Constants.PASSWORD, getPasswordfromUI()) ;
 
         /*Making a jsonObject request and handling response */
         JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, Constants.SIGNUP_URL, new JSONObject(params), new Response.Listener<JSONObject>() {
@@ -93,7 +137,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
                             break;
                         case "invalid" :
                             // Please try again
-                            checkErrors() ;
+                            HandleError() ;
                             break;
                         case "emailtaken" :
                             Log.i("Inside Log", "Email taken response" + response.toString()) ;
@@ -107,6 +151,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
                 }catch (JSONException e){
 
                     Log.e("EXCEPTION", "JSON " + e) ;
+                    Toast.makeText(getApplicationContext(), R.string.network_error, Toast.LENGTH_LONG).show();
                 }
 
 
@@ -122,7 +167,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
             public void onErrorResponse(VolleyError error) {
 
                 Log.e("Response Error", "Error: " + error) ;
-
+                Toast.makeText(getApplicationContext(), R.string.network_error, Toast.LENGTH_LONG).show();
             }
         });
 
@@ -134,17 +179,30 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
 
     }
 
-    /* Method to handle errors returned by API */
-    private void checkErrors() {
-
+    private void HandleError() {
+        //Handle "error" response
     }
 
-    boolean value ;
+    /* Method to handle errors returned by API */
+
+
+    /*Extraction of data as inserted by the user from UI */
+    public String getUsernamefromUI(){
+        return etUserName.getText().toString().trim() ;
+    }
+
+    public String getEmailfromUI(){
+        return etEmail.getText().toString().trim() ;
+    }
+
+    public String getPasswordfromUI(){
+        return etPassword.getText().toString().trim() ;
+    }
 
     /*api call to username duplicates */
     private boolean usernameCheck(){
 
-        USERCHECK_URL = String.format("https://swisapp.co:55555/api/useravailable?username=%s", username);
+        USERCHECK_URL = String.format("https://swisapp.co:55555/api/useravailable?username=%s", getUsernamefromUI());
 
         JsonObjectRequest jsonRequest2 = new JsonObjectRequest(Request.Method.GET, USERCHECK_URL, new JSONObject(), new Response.Listener<JSONObject>() {
             @Override
@@ -161,6 +219,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
             public void onErrorResponse(VolleyError error) {
 
                 Log.e("Response Error", "Error: " + error) ;
+                value = false ;
 
             }
         });
@@ -169,23 +228,17 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         RequestQueue requestQueue = Volley.newRequestQueue(this) ;
         requestQueue.add(jsonRequest2) ;
 
-
-
         return  value ;
     }
 
+    private boolean validityCheck() {
 
-    /*method of the class view.OnClickListener that is implemented*/
-    @Override
-    public void onClick(View v) {
-        if(v == bRegister) {
-            try {
-                boolean check = usernameCheck() ;
-                if(check)
-                    registration();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+        String Email = getEmailfromUI() ;
+
+        if(!Email.contains("@") || getPasswordfromUI().length() < 6 || getPasswordfromUI().contains(getUsernamefromUI())) {
+            return false ;
         }
+        else
+            return true ;
     }
 }
